e20813f1954bae2bd6f4014ae08aea6b63866a73
huynq55 <huynq23091@gmail.com>
2017-09-13 22:30:46 +0700

    Finish Lab01

--- JSE.Unit01.Basic.1/MyProgram.java
+++ JSE.Unit01.Basic.1/MyProgram.java
@@ -1,5 +1,8 @@
 public class MyProgram {
 	public static void main(String[] args) {
-		System.out.println("Say Hello to Java");
+		System.out.println("The number of arguments is " + args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("Value at " + i + " is " + args[i]);
+		}
 	}
 }
