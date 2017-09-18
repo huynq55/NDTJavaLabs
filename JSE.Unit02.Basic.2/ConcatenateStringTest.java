b8c008812c229afd36b8169e3f51118ffc598e9d
huynq55 <huynq_55@vnu.edu.vn>
2017-09-18 08:44:31 +0700

    hoan thanh lab02

--- JSE.Unit02.Basic.2/ConcatenateStringTest.java
+++ JSE.Unit02.Basic.2/ConcatenateStringTest.java
@@ -1,7 +1,35 @@
+// import java.lang.StringBuilder;
+
 public class ConcatenateStringTest {
   public static void main(String[] args) {
     int max = 10000;
     
+	// nhanh thu 2
+	StringBuilder builder = new StringBuilder();
+	long start = System.currentTimeMillis();
+	for (int i = 0; i < max; i++) {
+		builder.append(String.valueOf(i));
+	}
+	long end = System.currentTimeMillis();
+	System.out.println("time 1 = " + (end - start));
+	
+	// nhanh nhat
+	StringBuffer buffer = new StringBuffer();
+	start = System.currentTimeMillis();
+	for (int i = 0; i < max; i++) {
+		buffer.append(String.valueOf(i));
+	}
+	end = System.currentTimeMillis();
+	System.out.println("time 2 = " + (end - start));
+	
+	// cham nhat
+	String text = "";
+	start = System.currentTimeMillis();
+	for (int i = 0; i < max; i++) {
+		text += String.valueOf(i);
+	}
+	end = System.currentTimeMillis();
+	System.out.println("time 3 = " + (end - start));
     
     
     
